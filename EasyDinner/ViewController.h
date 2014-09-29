3e92d090d5ff4dbc4ed12bbeb0a7a7214d65ff8d
ZhouMeng <zhoumeng@mirakuyume.local>
2014-09-29 18:07:58 -0400

    Initial Commit

---
+++ EasyDinner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyDinner
+//
+//  Created by ZhouMeng on 14-9-29.
+//  Copyright (c) 2014年 DreamChou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EasyDinner/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EasyDinner
+//
+//  Created by ZhouMeng on 14-9-29.
+//  Copyright (c) 2014年 DreamChou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
